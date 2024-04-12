f90fd3ae6a8ed9c56e9c4d238abdd4bae8fce5a7
DzimaSh <dzmitry.shushkevich@gmail.com>
2024-04-12 22:12:15 +0300

    add client

--- client/src/main/java/by/dzimash/NumberClient.java
+++ client/src/main/java/by/dzimash/NumberClient.java
@@ -1,2 +1,49 @@
-package by.dzimash;public class NumberClient {
+package by.dzimash;
+
+import by.dzimash.observer.NumberObserver;
+import by.dzimash.service.NumberRequest;
+import by.dzimash.service.NumberServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class NumberClient {
+
+    private final NumberObserver numberObserver = new NumberObserver();
+
+    public static void main(String[] args) {
+        try {
+            new NumberClient().start();
+        } catch (InterruptedException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void start() throws InterruptedException {
+        log.info("Starting the client...");
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080)
+                .usePlaintext()
+                .build();
+
+        NumberServiceGrpc.NumberServiceStub stub = NumberServiceGrpc.newStub(channel);
+
+        NumberRequest request = NumberRequest.newBuilder()
+                .setFirstValue(0)
+                .setLastValue(30)
+                .build();
+
+        log.info("Sending request to server...");
+        stub.getNumbers(request, numberObserver);
+
+        int currentValue;
+        for (int i = 0; i < 50; i++) {
+            Thread.sleep(1000);
+            currentValue = numberObserver.getCurrentValue().incrementAndGet();
+            log.info("currentValue: " + currentValue);
+        }
+
+        log.info("Shutting down the client...");
+        channel.shutdown();
+    }
 }
--- client/src/main/java/by/dzimash/observer/NumberObserver.java
+++ client/src/main/java/by/dzimash/observer/NumberObserver.java
@@ -1,2 +1,34 @@
-package by.dzimash.observer;public class NumberObserver {
+package by.dzimash.observer;
+
+import by.dzimash.service.NumberResponse;
+import io.grpc.stub.StreamObserver;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Getter
+@Slf4j
+public class NumberObserver implements StreamObserver<NumberResponse> {
+
+    private final AtomicInteger currentValue = new AtomicInteger(0);
+
+    @Override
+    public void onNext(NumberResponse response) {
+        int lastNumberFromServer = response.getNumber();
+        int current = currentValue.addAndGet(lastNumberFromServer + 1);
+
+        log.info("currentValue: " + current);
+        log.info("number from server: " + lastNumberFromServer);
+    }
+
+    @Override
+    public void onError(Throwable t) {
+        log.error(t.getMessage());
+    }
+
+    @Override
+    public void onCompleted() {
+        log.info("Stream completed");
+    }
 }
